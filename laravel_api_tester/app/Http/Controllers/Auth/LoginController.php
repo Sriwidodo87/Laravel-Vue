888941094c426445b328f94ee825bb9c60cde493
WithDodo <swidodo87@gmail.com>
2025-10-05 14:35:07 +0800

    SPA Login

---
+++ laravel_api_tester/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        if(! Auth::attempt($request->only('email','password'))){
+
+            throw ValidationException::withMessages([
+                'email'=>['Your Credentials provided is incorrect']
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+    }
+}
--- laravel_api_tester/routes/web.php
+++ laravel_api_tester/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('auth')->group(function(){
+Route::prefix('Auth')->group(function(){
     Route::post('/login',LoginController::class);
 });
